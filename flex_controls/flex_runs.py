726c6d8c2be341350725508fbb2c0f721ab0db4c
Paul MacNichol <paulmacnichol@gmail.com>
2024-12-03 11:58:03 -0500

    updating get and post

--- flex_controls/flex_runs.py
+++ flex_controls/flex_runs.py
@@ -17,19 +17,32 @@ class FlexRuns:
         headers = {"Content-Type": "application/json"}
         headers.update(HEADERS)
         try:
-            # Prepare the payload if provided
-            if payload:
-                json_payload = json.dumps(payload) if payload else None
-                print(f"{method}\n{url},\n{HEADERS},\n{json_payload}\n")
-                response = requests.post(url=url, headers=HEADERS, data=json_payload)
+            if method == "POST":
+                if payload:
+                    json_payload = json.dumps(payload) if payload else None
+                    print(f"{method}\n{url},\n{HEADERS},\n{json_payload}\n")
+                    response = requests.post(
+                        url=url, headers=HEADERS, data=json_payload)
 
-            else:
-                response = requests.request(
-                    method=method,
-                    url=url,
-                    headers=HEADERS,
-                )
-            response.raise_for_status()  # Raise an exception for HTTP errors
+                else:
+                    response = requests.post(
+                        method=method,
+                        url=url,
+                        headers=HEADERS,
+                    )
+                response.raise_for_status()  # Raise an exception for HTTP errors
+            elif method == "GET":
+                if payload:
+                    json_payload = json.dumps(payload) if payload else None
+                    response = requests.post(
+                        url=url, headers=HEADERS, data=json_payload)
+                else:
+                    response = requests.post(
+                        method=method,
+                        url=url,
+                        headers=HEADERS,
+                    )
+                response.raise_for_status()  # Raise an exception for HTTP errors
             return response.json()
         except requests.exceptions.RequestException as e:
             print(f"Error sending {method} request to {url}: {e}")
@@ -46,7 +59,8 @@ class FlexRuns:
         response_json = FlexRuns._send_request("POST", runs_url, payload)
 
         if response_json:
-            run_id = [key for key in response_json["data"].keys() if "id" in key]
+            run_id = [key for key in response_json["data"].keys()
+                      if "id" in key]
             if run_id:
                 return run_id
             else:
@@ -73,7 +87,8 @@ class FlexRuns:
     ):
         protocol_file_payload = open(protocol_file_path, "rb")
         labware_file_payload = open(labware_file_path, "rb")
-        data = [("files", protocol_file_payload), ("files", labware_file_payload)]
+        data = [("files", protocol_file_payload),
+                ("files", labware_file_payload)]
         response = FlexRuns._send_request("POST", protocols_url, data)
         protocol_file_payload.close()
         labware_file_payload.close()
@@ -103,7 +118,8 @@ class FlexRuns:
     @staticmethod
     def pause_run(runs_url: str, run_id: int):
         actions_url = f"{runs_url}/{run_id}/actions"
-        action_payload = json.dumps({"data": {"actionType": FlexActions.PAUSE}})
+        action_payload = json.dumps(
+            {"data": {"actionType": FlexActions.PAUSE}})
         return FlexRuns._send_request("POST", actions_url, action_payload)
 
     @staticmethod
